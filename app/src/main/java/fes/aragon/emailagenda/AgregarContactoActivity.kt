c9893650a66c8af99964e79d5f18317908322c40
Luis Vallejo <luishalo117@hotmail.com>
2024-05-21 00:17:45 -0600

    Agregar la funcionalidad para agregar contactos.

--- app/src/main/java/fes/aragon/emailagenda/AgregarContactoActivity.kt
+++ app/src/main/java/fes/aragon/emailagenda/AgregarContactoActivity.kt
@@ -1,13 +1,9 @@
 package fes.aragon.emailagenda
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
