4b6a5527d2f481c8afac6e32710c6d9adcee5304
Luis Vallejo <luishalo117@hotmail.com>
2024-05-21 00:16:39 -0600

    Agregar la funcionalidad para agregar contactos.

--- app/src/main/java/fes/aragon/emailagenda/AgregarContactoActivity.kt
+++ app/src/main/java/fes/aragon/emailagenda/AgregarContactoActivity.kt
@@ -1,18 +1,52 @@
 package fes.aragon.emailagenda
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 import fes.aragon.emailagenda.databinding.ActivityAgregarContactoBinding
 
 class AgregarContactoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAgregarContactoBinding
+    private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityAgregarContactoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        save()
+    }
+
+    private fun save() {
+        val user = FirebaseAuth.getInstance().currentUser
+        val email = user?.email
+        val contactosRef = db.collection("usrs").document(email.toString())
+            .collection("contactos")
+
+        binding.agregarContactoBtn.setOnClickListener {
+            val contacto = hashMapOf(
+                "correo" to binding.correoContacto.text.toString(),
+                "nombre" to binding.nombreContacto.text.toString(),
+                "telefoto" to binding.telefonoContacto.text.toString(),
+            )
+
+            contactosRef.add(contacto).addOnCompleteListener {
+                Toast.makeText(this, "Contacto agregado", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Error al agregar el contacto", Toast.LENGTH_SHORT).show()
+            }
+
+            // Limpiar los campos después de agregar el contacto
+            binding.correoContacto.text = ""
+            binding.nombreContacto.text = ""
+            binding.telefonoContacto.text = ""
+        }
     }
 }
